81ac77fb0c4b506f88599b86484c57fedcbe095d
Steven <SeiryuZ@hotmail.com>
2016-06-21 15:28:44 +0100

    Subprocess.run is not available on python 3.4

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -35,9 +35,9 @@ def run_job(job_instance):
 
         with open(job_instance.get_log_file_path('stdout'), 'w') as stdout_file:
             with open(job_instance.get_log_file_path('stderr'), 'w') as stderr_file:
-                completed = subprocess.run(command,
-                                           stdout=stdout_file,
-                                           stderr=stderr_file)
+                completed = subprocess.call(command,
+                                            stdout=stdout_file,
+                                            stderr=stderr_file)
 
                 # Update the status of job accordingly
                 if completed.returncode == 0:
